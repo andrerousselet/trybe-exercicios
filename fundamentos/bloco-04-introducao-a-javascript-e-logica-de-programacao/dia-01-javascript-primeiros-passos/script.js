86805686994fdb4370570652f5e3cb25bb319f18
Andre Rousselet <andre098@hotmail.com>
2022-02-02 20:00:04 -0300

    Exercicio 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -49,7 +49,25 @@ if (num > 0) {
 
 
 // Exercicio 7
+function converteNota(porcentagem) {
+  let nota = '';
 
+  if (porcentagem >= 90) {
+    nota = 'A';
+  } else if (porcentagem >= 80) {
+    nota = 'B';
+  } else if (porcentagem >= 70) {
+    nota = 'C';
+  } else if (porcentagem >= 60) {
+    nota = 'D';
+  } else if (porcentagem >= 50) {
+    nota = 'E';
+  } else {
+    nota = 'F';
+  }
+  return nota
+}
+console.log(converteNota(80));
 
 // Exercicio 8
 
