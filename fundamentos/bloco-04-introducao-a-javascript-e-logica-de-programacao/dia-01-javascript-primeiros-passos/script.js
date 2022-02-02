3f9a905536edd7ba290a9bec3ebd4261528a24fe
Andre Rousselet <andre098@hotmail.com>
2022-02-02 18:31:31 -0300

    Exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -1,3 +1,4 @@
+// Exercicio 1
 const a = 9;
 const b = 3;
 
@@ -6,3 +7,13 @@ let subtraction = a - b;
 let multiplication = a * b;
 let division = a / b;
 let resto = a % b;
+
+// Exercicio 2
+const c = 5;
+const d = 10;
+
+if (c > d) {
+  console.log(c);
+} else {
+  console.log(d);
+}
