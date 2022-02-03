29455acef36276832a6fe3198e4017ec002f6103
Andre Rousselet <andre098@hotmail.com>
2022-02-03 18:58:12 -0300

    Adiciona requisito 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -14,6 +14,7 @@ for (let numero of numbers) {
 };
 console.log(sum);
 
+
 //Exercicio 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
@@ -23,6 +24,7 @@ for (let numero of numbers) {
 // console.log(sum);
 // console.log(numbers.length);
 console.log(`Média aritmética igual a ${sum / numbers.length}`);
+
 
 //Exercicio 4
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -37,3 +39,13 @@ if ((sum / numbers.length) > 20) {
   console.log(`↳ Valor menor ou igual a 20`);
 };
 
+
+//Exercicio 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let biggestNumber = 0;
+for (i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > biggestNumber) {
+    biggestNumber = numbers[i];
+  }
+}
+console.log(biggestNumber);
