1cb83f8e96172116dd9d3f4b239b7e9a92ac8b52
Andre Rousselet <andre098@hotmail.com>
2022-02-03 11:54:35 -0300

    Adiciona exercicio 9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -114,7 +114,15 @@ if ((num1 % 2) === 0 || (num2 % 2) === 0 || (num3 % 2) === 0) {
 };
 
 // Exercicio 9
+const num1 = 4;
+const num2 = 8;
+const num3 = 10;
 
+if ((num1 % 2) !== 0 || (num2 % 2) !== 0 || (num3 % 2) !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+};
 
 // Exercicio 10
 
