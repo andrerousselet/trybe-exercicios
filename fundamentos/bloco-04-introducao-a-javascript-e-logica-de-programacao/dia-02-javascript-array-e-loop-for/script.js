5b4c8488b240eeaefb20662a28486c1e575b3e98
Andre Rousselet <andre098@hotmail.com>
2022-02-03 18:08:01 -0300

    Adiciona requisito 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -13,3 +13,13 @@ for (let numero of numbers) {
   sum += numero;
 };
 console.log(sum);
+
+//Exercicio 3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let numero of numbers) {
+  sum += numero;
+};
+// console.log(sum);
+// console.log(numbers.length);
+console.log(sum / numbers.length);
