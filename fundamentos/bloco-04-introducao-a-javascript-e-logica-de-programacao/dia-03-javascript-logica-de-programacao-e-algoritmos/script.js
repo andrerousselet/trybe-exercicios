8b1397d14b378c4d69a8fc6783a428f8b1c0a1aa
Andre Rousselet <andre098@hotmail.com>
2022-02-04 20:10:56 -0300

    Adiciona exercicio 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,9 @@
 //Exercicio 1
 let fatorial = 10;
 for (let index = fatorial - 1; index >= 1; index -= 1) {
-  console.log(fatorial);
+  // console.log(fatorial);
   fatorial *= index;
-  console.log(index);
+  // console.log(index);
 };
 console.log(fatorial);
 
@@ -16,4 +16,34 @@ for (let index = word.length - 1; index >= 0; index -= 1) {
 };
 console.log(inverted);
 
+
+//Exercicio 3.1
+// Achando a maior palavra na array
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let numberOfChar = 0;
+let biggestWord = '';
+for (let index = 0; index < array.length; index += 1) {
+  if ((array[index].length) > numberOfChar) {
+    numberOfChar = array[index].length;
+  }
+  if (numberOfChar === array[index].length) {
+    biggestWord = array[index];
+  }
+}
+console.log(biggestWord);
+
+//Exercicio 3.2
+// Achando a menor palavra na array
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let numberOfChar = 50;
+let smallestWord = '';
+for (let index = 0; index < array.length; index += 1) {
+  if ((array[index].length) < numberOfChar) {
+    numberOfChar = array[index].length;
+  }
+  if (numberOfChar === array[index].length) {
+    smallestWord = array[index];
+  }
+}
+console.log(smallestWord);
 
