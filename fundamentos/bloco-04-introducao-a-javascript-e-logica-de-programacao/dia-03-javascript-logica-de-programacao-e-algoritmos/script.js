23c8f041eacdea640e59ead018c9c105aa2bba8c
Andre Rousselet <andre098@hotmail.com>
2022-02-04 19:32:19 -0300

    Adiciona exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -7,3 +7,13 @@ for (let index = fatorial - 1; index >= 1; index -= 1) {
 };
 console.log(fatorial);
 
+
+//Exercicio 2
+let word = 'tryber';
+let inverted = '';
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  inverted += word[index];
+};
+console.log(inverted);
+
+
