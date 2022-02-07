bbe5efb4bf63ed1c65a9f2346bf80f131107bc72
Andre Rousselet <andre098@hotmail.com>
2022-02-07 19:23:03 -0300

    Adiciona exercicio 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -20,3 +20,36 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 };
+
+//Exercicio 5
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+for (let key in info) {
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+};
+
+
+
+//Exercicio 6
+/*
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+*/
