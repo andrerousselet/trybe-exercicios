dc093fd74a0872a13f217e04b5328caad9b4da6d
Andre Rousselet <andre098@hotmail.com>
2022-02-07 19:40:19 -0300

    Adiciona exercicio 7 e 8

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -51,3 +51,14 @@ let leitor = {
   ],
 };
 console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama \'' + leitor.livrosFavoritos[0].titulo + '\'.');
+
+//Exercicio 7
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+console.log(leitor);
+
+//Exercicio 8
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
