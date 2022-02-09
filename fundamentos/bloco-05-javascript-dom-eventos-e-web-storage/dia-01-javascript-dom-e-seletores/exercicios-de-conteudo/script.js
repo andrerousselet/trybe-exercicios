b42ba7f42308e7f2f3389d05fe87cc1f408d5217
Andre Rousselet <andre098@hotmail.com>
2022-02-09 16:04:48 -0300

    Treina função getElementByID

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-de-conteudo/script.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("paragraph");
+
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Um Sonho de Liberdade';
+
+document.getElementById('second-paragraph').innerText = 'Esse filme é um dos melhores de todos os tempos; conta com atuações magistrais e possui uma história linda sobre como lidar com a vida "como ela é" e fazer o melhor que seja possível.';
+
+document.getElementById('subtitle').innerText = 'Grande filme sobre superação';
