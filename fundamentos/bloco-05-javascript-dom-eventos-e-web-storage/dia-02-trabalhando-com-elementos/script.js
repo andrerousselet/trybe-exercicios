f7dabaeec1f918f70675c1315da76af700f27c2d
Andre Rousselet <andre098@hotmail.com>
2022-02-10 19:05:06 -0300

    Adiciona exercicio 2 atribuindo classe com className e usando appendChild de novo

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -1,5 +1,10 @@
-//Exercicio 1
+//Exercicio 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body
 let body = document.querySelector('body');
 let h1Title = document.createElement('h1');
 h1Title.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1Title);
+
+//Exercicio 2 - Adicione a tag main com a classe main-content como filho da tag body
+let main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
