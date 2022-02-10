58218f7762224f3b5c9d85118101944edd80aff6
Andre Rousselet <andre098@hotmail.com>
2022-02-10 18:59:52 -0300

    Adiciona exercicio 1 usando appendChild

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -0,0 +1,5 @@
+//Exercicio 1
+let body = document.querySelector('body');
+let h1Title = document.createElement('h1');
+h1Title.innerText = 'Exercício 5.2 - JavaScript DOM';
+body.appendChild(h1Title);
