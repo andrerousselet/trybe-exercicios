a3a8043aca5abb85e9a326018b50e1216c031d04
Andre Rousselet <andre098@hotmail.com>
2022-02-14 20:24:59 -0300

    Exercicio 2: cria botão com string de parâmetro como nome

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -14,20 +14,21 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 //Exercicio 1
+let decDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
 function createDaysOfTheMonth() {
-  const decDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  const monthDaysList = document.querySelector('#days');
+  let monthDaysList = document.querySelector('#days');
   
   for (let index = 0; index < decDaysList.length; index += 1) {
-    const days = decDaysList[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
+    let day = decDaysList[index];
+    let dayListItem = document.createElement('li');
+    dayListItem.innerHTML = day;
     dayListItem.className = 'day';
 
-    if (days === 24 || days === 25 || days === 31) {
+    if (day === 24 || day === 25 || day === 31) {
       dayListItem.classList.add('holiday');
     }
-    if (days === 4 || days === 11 || days === 18 || days === 25) {
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
       dayListItem.classList.add('friday');
     }
     
@@ -36,3 +37,14 @@ function createDaysOfTheMonth() {
 };
 
 createDaysOfTheMonth()
+
+//Exercicio 2
+function createHolidaysButton(name) {
+  let btnContainer = document.querySelector('.buttons-container');
+  let holidayBtn = document.createElement('button');
+  holidayBtn.id = 'btn-holiday';
+  holidayBtn.innerHTML = name;
+  btnContainer.appendChild(holidayBtn);
+}
+
+createHolidaysButton('Feriados');
