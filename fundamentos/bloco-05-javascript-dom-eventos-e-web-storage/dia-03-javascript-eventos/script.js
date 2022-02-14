c48e91e4b2fd8701aa0603e7ce8732e8b25c0a29
Andre Rousselet <andre098@hotmail.com>
2022-02-14 19:59:10 -0300

    Exercicio 1: adiciona dias do mês - 29 a 31

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -13,4 +13,20 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+//Exercicio 1
+function createDaysOfTheMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDaysList = document.querySelector('#days');
+  
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const days = dezDaysList[index];
+    console.log(dezDaysList[index]);
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+    dayListItem.className = 'day';
+    
+    monthDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheMonth()
