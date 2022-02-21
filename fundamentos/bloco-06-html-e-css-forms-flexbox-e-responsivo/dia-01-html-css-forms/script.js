3f6c1bdce4dd5fc81be19d0fa0a16cd5ecfd7480
Andre Rousselet <andre098@hotmail.com>
2022-02-21 16:46:50 -0300

    Adiciona função para limpar inputs

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -2,7 +2,20 @@ function submit(event) {
   event.preventDefault();
 }
 
+function clear() {
+  const inputs = document.querySelectorAll('input');
+  let textArea = document.querySelector('textarea');
+  for (let index = 0; index < inputs.length; index += 1) {
+    let userInput = inputs[index];
+    userInput.value = '';
+    userInput.checked = false;
+  }
+  textArea.value = '';
+}
+
 window.onload = function () {
-  const button = document.getElementById('btn');
-  button.addEventListener('click', submit);
+  const submitButton = document.getElementById('btn-submit');
+  const clearButton = document.getElementById('btn-clear');
+  submitButton.addEventListener('click', submit);
+  clearButton.addEventListener('click', clear);
 }
