69f85d97dffe610e9b432e3da3a8682890698f2d
Andre Rousselet <andre098@hotmail.com>
2022-02-21 18:18:22 -0300

    Adiciona função do ultimo exercicio

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -2,8 +2,29 @@ const submitButton = document.getElementById('btn-submit');
 const clearButton = document.getElementById('btn-clear');
 const auth = document.getElementById('auth');
 
+function textInputValidation() {
+  const email = document.querySelector('#email').value.length;
+  const invalidEmail = email < 10 || email > 50;
+  const name = document.querySelector('#username').value.length;
+  const invalidName = name < 10 || name > 40;
+  const reason = document.querySelector('#porque').value.length;
+  const invalidReason = reason > 500;
+  if (invalidEmail || invalidName || invalidReason) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 function submit(event) {
   event.preventDefault();
+  const validation = textInputValidation();
+  if (validation === false) {
+    alert('Dados inválidos');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
 }
 
 function clear() {
