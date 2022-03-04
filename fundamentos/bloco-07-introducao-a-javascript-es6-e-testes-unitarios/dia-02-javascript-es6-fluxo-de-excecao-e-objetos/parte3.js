235f9bdde9f6d6c900967adf6a9d9646f8d6f3a0
Andre Rousselet <andre098@hotmail.com>
2022-03-04 19:02:44 -0300

    Parte 3, exercicio 1

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const function1 = (obj, key, value) => {
+  obj.key = value;
+  return obj;
+}
+
+console.log(function1(lesson2, 'turno', 'noite'));
