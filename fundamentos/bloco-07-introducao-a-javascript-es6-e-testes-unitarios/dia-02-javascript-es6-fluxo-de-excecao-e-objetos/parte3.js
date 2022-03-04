6fb78d3376b8c34452f09d8b5b97d16ce800b9f4
Andre Rousselet <andre098@hotmail.com>
2022-03-04 19:48:48 -0300

    Parte 3, exercicio 5

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
@@ -25,22 +25,30 @@ const function1 = (obj, key, value) => {
 }
 console.log(function1(lesson2, 'turno', 'noite'));
 
-console.log(`
-`);
+console.log(`---`);
 
 //Exercicio 2
 const function2 = (obj) => Object.keys(obj).forEach((key) => console.log(`${key}`));
 function2(lesson3);
 
-console.log(`
-`);
+console.log(`---`);
 
 //Exercicio 3
 const function3 = (obj) => console.log(Object.keys(obj).length);
 function3(lesson1);
 
-console.log(`
-`);
+console.log(`---`);
 
+//Exercicio 4
 const function4 = (obj) => Object.values(obj).forEach((value) => console.log(`${value}`));
 function4(lesson3);
+
+console.log(`---`);
+
+//Exercicio 5
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})
+console.log(allLessons);
+
+console.log(`---`);
+
+//Exercicio 6
