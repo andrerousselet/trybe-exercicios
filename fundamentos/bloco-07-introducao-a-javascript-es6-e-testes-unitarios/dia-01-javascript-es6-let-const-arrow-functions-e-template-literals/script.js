71e625432e36c3923fc5be6ca943f3bfac5819fd
Andre Rousselet <andre098@hotmail.com>
2022-03-04 12:04:19 -0300

    Parte 2, exercicio 3 (index.html)

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -67,3 +67,4 @@ console.log(longestWord(text));
 * const longestWord = text => text.split(' ').sort((a, b) => b.length - a.length)[0];
 * console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
 */
+
