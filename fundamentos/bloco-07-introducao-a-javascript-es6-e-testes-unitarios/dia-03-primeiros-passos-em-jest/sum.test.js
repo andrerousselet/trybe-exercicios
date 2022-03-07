9d290693768af149e9e0cc1319173a3f8e68da6c
Andre Rousselet <andre098@hotmail.com>
2022-03-07 19:06:22 -0300

    Finaliza exercicio 1.2, 1.3 e 1.4

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,16 @@
+//1.1
 const sum = require('./sum');
 
 it('Expect sum(4, 5) to equal 9', () => {
   expect(sum(4, 5)).toBe(9)
+});
+
+//1.2
+it('Expect sum(0, 0) to equal 0', () => {
+  expect(sum(0, 0)).toBe(0)
+});
+
+//1.3 e 1.4
+it('Expect sum(4, "5") to throw Error', () => {
+  expect(() => sum(4, '5')).toThrow('parameters must be numbers')
 });
