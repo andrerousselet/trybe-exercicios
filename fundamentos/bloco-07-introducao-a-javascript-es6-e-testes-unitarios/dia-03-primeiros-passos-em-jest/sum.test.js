942d780690a297fc803154c9019175145aaf69bf
Andre Rousselet <andre098@hotmail.com>
2022-03-07 19:15:36 -0300

    Refatora código usando describe, conforme gabarito

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,16 +1,14 @@
 //1.1
 const sum = require('./sum');
 
-it('Expect sum(4, 5) to equal 9', () => {
-  expect(sum(4, 5)).toBe(9)
-});
-
-//1.2
-it('Expect sum(0, 0) to equal 0', () => {
-  expect(sum(0, 0)).toBe(0)
-});
-
-//1.3 e 1.4
-it('Expect sum(4, "5") to throw Error', () => {
-  expect(() => sum(4, '5')).toThrow('parameters must be numbers')
+describe('Tests function sum', () => {
+  it('Expect sum(a, b) to equal a + b', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);//1.2
+  });  
+  //1.3 e 1.4
+  it('Expect to throw Error if one parameter is string', () => {
+    expect(() => sum(4, '5')).toThrow();
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  });
 });
