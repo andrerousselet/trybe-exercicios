a2307bd311e710f7b6fa5d68c17cff873b5aff8a
Andre Rousselet <andre098@hotmail.com>
2022-03-11 14:35:25 -0300

    Exercicio 3 usando find

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio3.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio3.js
@@ -61,17 +61,18 @@ const books = [
   },
 ];
 
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
 
 function getNamedBook() {
-  // escreva seu código aqui
+  return books.find((book) => book.name.length === 26);
 }
+console.log(getNamedBook());
