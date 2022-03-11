3801769cb376949fd565b9b45a235c3c498e1878
Andre Rousselet <andre098@hotmail.com>
2022-03-11 15:00:10 -0300

    Exercicio 5 usando every

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio5.js
@@ -64,5 +64,6 @@ const books = [
 const expectedResult = false;
 
 function everyoneWasBornOnSecXX() {
-  // escreva seu código aqui
+  return books.every((book) => book.author.birthYear >= 1900 && book.author.birthYear < 2000);
 }
+console.log(everyoneWasBornOnSecXX());
