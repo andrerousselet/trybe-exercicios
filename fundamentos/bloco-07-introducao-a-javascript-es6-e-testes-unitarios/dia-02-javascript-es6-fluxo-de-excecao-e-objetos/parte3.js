2ebf6c5ef759b40e3ac9e9e6208f2cd9cefd7641
Andre Rousselet <andre098@hotmail.com>
2022-03-14 11:25:50 -0300

    Parte 3, exercicio 7 e 8

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
@@ -18,6 +18,8 @@ const lesson3 = {
   turno: 'noite',
 };
 
+console.log(`-1-`);
+
 // //Exercicio 1
 const function1 = (obj, key, value) => {
   obj[key] = value;
@@ -25,31 +27,31 @@ const function1 = (obj, key, value) => {
 }
 console.log(function1(lesson2, 'turno', 'noite'));
 
-console.log(`---`);
+console.log(`-2-`);
 
 //Exercicio 2
 const function2 = (obj) => Object.keys(obj).forEach((key) => console.log(`${key}`));
 function2(lesson3);
 
-console.log(`---`);
+console.log(`-3-`);
 
 //Exercicio 3
 const function3 = (obj) => console.log(Object.keys(obj).length);
 function3(lesson1);
 
-console.log(`---`);
+console.log(`-4-`);
 
 //Exercicio 4
 const function4 = (obj) => Object.values(obj).forEach((value) => console.log(`${value}`));
 function4(lesson3);
 
-console.log(`---`);
+console.log(`-5-`);
 
 //Exercicio 5
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})
 console.log(allLessons);
 
-console.log(`---`);
+console.log(`-6-`);
 
 //Exercicio 6
 const function6 = () => {
@@ -59,5 +61,26 @@ const function6 = () => {
   });
   return sum;
 }
-
 console.log(function6());
+
+console.log(`-7-`);
+
+//Exercicio 7
+const function7 = (key, index) => {
+  const prop = allLessons[key];
+  return Object.values(prop)[index];
+};
+console.log(function7('lesson1', 0));
+
+console.log(`-8-`);
+
+//Exericio 8
+const function8 = (obj, key, value) => {
+  const array = Object.entries(obj);
+  let result = false;
+  for (let item in array) {
+    if (array[item][0] === key && array[item][1] === value) result = true;
+  }
+  return result;
+}
+console.log(function8(lesson3, 'professor', 'Maria Clara'));
