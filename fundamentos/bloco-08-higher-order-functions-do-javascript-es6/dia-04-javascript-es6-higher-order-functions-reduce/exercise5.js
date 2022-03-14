5077cb33341e0cd35314231b6937922d90ef9880
Andre Rousselet <andre098@hotmail.com>
2022-03-14 18:03:13 -0300

    Exercicio 5 usando reduce e for/of e também reduce com reduce

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -4,6 +4,23 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-function containsA() {
-  // escreva seu código aqui
+// function containsA(arr) {
+//   const newArray = arr.reduce((acc, element) => `${acc}${element}`).split('');
+//   let sum = 0;
+//   for (let letter of newArray) {
+//     if (letter === 'a' || letter === 'A') sum += 1;
+//   }
+//   return sum;
+// }
+
+// console.log(containsA(names));
+
+function containsA(arr) {
+  const newArray = arr.reduce((acc, element) => `${acc}${element}`).split('');
+  return newArray.reduce((acc, letter) => {
+    if (letter === 'a' || letter === 'A') acc += 1;
+    return acc;
+  }, 0)
 }
+
+console.log(containsA(names));
