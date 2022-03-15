34bc087481b8b44c71cbb7da56fb8404675e198d
Andre Rousselet <andre098@hotmail.com>
2022-03-15 18:50:49 -0300

    Exercicio 2 usando rest e reduce

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2sum.js
@@ -0,0 +1,6 @@
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+const sum = (...param) => param.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum(1,2,3)) // 6
+console.log(sum(1,2,3,4,5)); // 15
+console.log(sum(1,2,3,4,5,6,7,8,9,10)) // 55
