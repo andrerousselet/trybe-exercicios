a0b6c199e841bb18b5365aea79279cf4f2dfe5f5
Andre Rousselet <andre098@hotmail.com>
2022-03-24 12:44:26 -0300

    Exercicio 2: cria funções baseadas no live-lectures

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/coincap-api.js
@@ -0,0 +1,36 @@
+const url = `https://api.coincap.io/v2/assets`;
+
+const append = (crypto) => {
+  const ul = document.querySelector('#crypto-list');
+  const li = document.createElement('li');
+  li.innerHTML = `${crypto.name} (${crypto.symbol}): ${crypto.price}`;
+  ul.appendChild(li);
+}
+
+const extractCryptoInfo = (data) => {
+  const crypto = {
+    name: data.name,
+    symbol: data.symbol,
+    price: data.priceUsd
+  }
+  return crypto;
+}
+
+const formatArray = (data) => {
+  return data.map((element) => extractCryptoInfo(element));
+}
+
+const fetchCrypto = async () => {
+  try {
+    const response = await fetch(url);
+    const cryptoObj = await response.json();
+    const arrayOfCryptos = cryptoObj.data;
+    const formattedArrayOfCryptos = formatArray(arrayOfCryptos);
+    formattedArrayOfCryptos.forEach((crypto) => append(crypto));
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+
+window.onload = fetchCrypto();
