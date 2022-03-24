8c386f7d3f4aa744471f4ffe8d32f49bfc519fa0
Andre Rousselet <andre098@hotmail.com>
2022-03-24 12:53:39 -0300

    Adicoina filter na função formatArray

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/coincap-api.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/coincap-api.js
@@ -11,13 +11,16 @@ const extractCryptoInfo = (data) => {
   const crypto = {
     name: data.name,
     symbol: data.symbol,
-    price: data.priceUsd
+    price: data.priceUsd,
+    rank: data.rank
   }
   return crypto;
 }
 
 const formatArray = (data) => {
-  return data.map((element) => extractCryptoInfo(element));
+  return data
+    .map((element) => extractCryptoInfo(element))
+    .filter((element) => element.rank <= 10);
 }
 
 const fetchCrypto = async () => {
@@ -26,11 +29,11 @@ const fetchCrypto = async () => {
     const cryptoObj = await response.json();
     const arrayOfCryptos = cryptoObj.data;
     const formattedArrayOfCryptos = formatArray(arrayOfCryptos);
+    console.log(formattedArrayOfCryptos);
     formattedArrayOfCryptos.forEach((crypto) => append(crypto));
   } catch (error) {
     console.log(error);
   }
-  
 }
 
 window.onload = fetchCrypto();
