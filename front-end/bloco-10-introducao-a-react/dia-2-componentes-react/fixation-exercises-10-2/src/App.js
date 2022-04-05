c72a19053990cbad8225281edc9959d8c44ef8bf
Andre Rousselet <andre098@hotmail.com>
2022-04-05 16:02:42 -0300

    Exercicios de fixacao: composicao de componentes

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import Image from './Image';
+// import Image from './Image';
+import Album from './Album';
 import './App.css';
 
-function App() {
-  return (
-    <main>
-      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
-    </main>
-  );
+//Parte - Props
+// function App() {
+//   return (
+//     <main>
+//       <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+//     </main>
+//   );
+// }
+
+//Parte - Composição de Componentes
+class App extends React.Component {
+  render() {
+    const album01 = {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+    return (
+      <div>
+        <Album album={album01}/>
+        <Album album={album02}/>
+      </div>
+    );
+  }
 }
 
 export default App;
