823478b5fa32e439e7cdac2461ddc18577fc21b0
Andre Rousselet <andre098@hotmail.com>
2022-04-05 08:48:38 -0300

    Exercicio 3: Header e Content

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my_app2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+
+function App() {
+  return (
+    <><Header /><Content /></>
+  );
+}
+
+export default App;
