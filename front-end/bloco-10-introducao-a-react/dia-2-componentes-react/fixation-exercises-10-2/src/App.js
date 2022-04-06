3756875096b73342f7b3b3f8d3e90b99ecadf62b
Andre Rousselet <andre098@hotmail.com>
2022-04-06 10:33:06 -0300

    Exercicios de fixacao: composicao de componentes 2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Image from './Image';
-import Album from './Album';
+// import Album from './Album';
+import Order from './Order';
 import './App.css';
 
 //Parte - Props
@@ -12,43 +13,76 @@ import './App.css';
 //   );
 // }
 
-//Parte - Composição de Componentes
+//Parte - Composição de Componentes - 1
+// class App extends React.Component {
+//   render() {
+//     const album01 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+//       title: 'Mylo Xyloto',
+//       releaseDate: {
+//         year: '2011',
+//         month: '10',
+//         day: '24',
+//       },
+//       others: {
+//         recordCompany: 'Capitol, Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+
+//     const album02 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+//       title: 'Ghost Stories',
+//       releaseDate: {
+//         year: '2014',
+//         month: '05',
+//         day: '16',
+//       },
+//       others: {
+//         recordCompany: 'Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+//     return (
+//       <div>
+//         <Album album={album01}/>
+//         <Album album={album02}/>
+//       </div>
+//     );
+//   }
+// }
+
+//Parte - Composição de Componentes - 2
 class App extends React.Component {
   render() {
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
-      },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
       }
     };
 
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
-      },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
       }
     };
+
     return (
-      <div>
-        <Album album={album01}/>
-        <Album album={album02}/>
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
       </div>
-    );
-  }
-}
+    )
+  };
+};
 
 export default App;
