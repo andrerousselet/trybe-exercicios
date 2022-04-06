f76e0bd6d326ca434f6ad57aa214a60d5dffe4c9
Andre Rousselet <andre098@hotmail.com>
2022-04-06 11:30:38 -0300

    Exercicios de fixacao: lista de componentes e chaves - usando prop key

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -55,31 +55,31 @@ import './App.css';
 //Parte - Composição de Componentes - 2
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
+    const items = [
+      {
+        id: 102,
+        user: "cena@gmail.com",
+        product: "Razer Headphone",
+        price: {
+          value: 99.99,
+          currency: "dollars"
+        }
+      },
+      {
+        id: 77,
+        user: "cena@gmail.com",
+        product: "Monster 500mL",
+        price: {
+          value: 9.99,
+          currency: "dollars"
+        }
       }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+    ];
 
     return (
       <div className="App">
         <h1> Orders recently created </h1>
-        <Order order={headphone} />
-        <Order order={energyDrink} />
+        {items.map((item) => <Order key={item.id} order={item} />)}
       </div>
     )
   };
