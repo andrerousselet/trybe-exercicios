dff887cc3c43f915f178983a9ecbcc7b96bc3715
Andre Rousselet <andre098@hotmail.com>
2022-04-07 18:47:39 -0300

    Para fixar 3 e 4

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicio-de-fixacao/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicio-de-fixacao/fancy-buttons/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './App.css';
 
-const handleClick1 = () => console.log('Botão 1 foi clicado!')
-const handleClick2 = () => console.log('Botão 2 foi clicado!')
-const handleClick3 = () => console.log('Botão 3 foi clicado!')
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+  }
+  
+  handleClick1() {
+    console.log('Botão 1 foi clicado!', this)
+  }
+  handleClick2() {
+    console.log('Botão 2 foi clicado!', this)
+  }
+  handleClick3() {
+    console.log('Botão 3 foi clicado!', this)
+  }
+
   render(){
     return (
       <div>
-        <button onClick={handleClick1}>BOTÃO 1</button>
-        <button onClick={handleClick2}>BOTÃO 2</button>
-        <button onClick={handleClick3}>BOTÃO 3</button>
+        <button type="button" onClick={this.handleClick1}>BOTÃO 1</button>
+        <button type="button" onClick={this.handleClick2}>BOTÃO 2</button>
+        <button type="button" onClick={this.handleClick3}>BOTÃO 3</button>
       </div>
     );
   }
