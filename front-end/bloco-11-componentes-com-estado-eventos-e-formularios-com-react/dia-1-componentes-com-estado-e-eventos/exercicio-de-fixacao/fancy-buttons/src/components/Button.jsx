2ca6edc286f9430d736ba90d3af9b6d2490746c5
Andre Rousselet <andre098@hotmail.com>
2022-04-11 12:03:56 -0300

    Altera lógica de verificação para dentro do prop style

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicio-de-fixacao/fancy-buttons/src/components/Button.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicio-de-fixacao/fancy-buttons/src/components/Button.jsx
@@ -14,17 +14,23 @@ class Button extends React.Component {
     this.setState((prevState, _props) => ({
       numberOfClicks: prevState.numberOfClicks + 1
     }))
-    if ((this.state.numberOfClicks + 1) % 2 === 0) {
-      e.target.style.backgroundColor = 'lightseagreen';
-    } else {
-      e.target.style.backgroundColor = '';
-    }
+    // if ((this.state.numberOfClicks + 1) % 2 === 0) {
+    //   e.target.style.backgroundColor = 'lightseagreen';
+    // } else {
+    //   e.target.style.backgroundColor = '';
+    // }
   }
 
   render() {
     return (
       <span>
-        <button type="button" onClick={this.handleClick}>{this.state.numberOfClicks}</button>
+        <button
+          type="button"
+          style={{ backgroundColor: `${this.state.numberOfClicks % 2 === 0 ? 'lightseagreen' : ''}` }}
+          onClick={this.handleClick}
+        >
+          {this.state.numberOfClicks}
+        </button>
       </span>
     );
   }
