121638f5e9ee88dc0a86bd99445d9a08e074c69a
Andre Rousselet <andre098@hotmail.com>
2022-07-26 19:11:40 -0300

    exercise 3: makes program more sociable with interaction using readline-sync lib

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc-calc/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc-calc/imc.js
@@ -1,5 +1,14 @@
-function calcImc(height, weight) {
-  return (weight / Math.pow(height, 2)).toFixed(2);
+const readline = require('readline-sync');
+
+function calcImc() {
+  const name = readline.question('Olá! Esse pequeno programa vai calcular o seu IMC. Vamos lá!!\n\nPrimeiro, qual o seu nome? ');
+
+  const weight = readline.questionFloat('Qual o seu peso? ');
+  const height = readline.questionFloat('Qual a sua altura? ');
+
+  const imc = (weight / Math.pow(height, 2)).toFixed(2);
+
+  console.log(`\nBom...${name}, você nos disse que seu peso é ${weight}kg e sua altura é ${height}m\nEntão, seu IMC é ${imc}`);
 }
 
-console.log(calcImc(1.72, 70));
+calcImc();
