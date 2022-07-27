17dd00effe22b73c6867c34b50a2606631f95262
Andre Rousselet <andre098@hotmail.com>
2022-07-27 15:08:28 -0300

    exercise 5: use ifs to find weight category

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc-calc/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc-calc/imc.js
@@ -8,7 +8,20 @@ function calcImc() {
 
   const imc = (weight / Math.pow(height, 2)).toFixed(2);
 
-  console.log(`\nBom...${name}, você nos disse que seu peso é ${weight}kg e sua altura é ${height}m\nEntão, seu IMC é ${imc}`);
+  function findCategory(calculatedImc) {
+    if (calculatedImc >= 40) return 'Obesidade graus III e IV';
+    if (calculatedImc >= 35 && calculatedImc < 40) return 'Obesidade grau II';
+    if (calculatedImc >= 30 && calculatedImc < 35) return 'Obesidade grau I';
+    if (calculatedImc >= 25 && calculatedImc < 30) return 'Acima do peso (sobrepeso)';
+    if (calculatedImc < 18.5) return 'Abaixo do peso (magreza)';
+    return 'Peso normal';
+  }
+
+  const category = findCategory(Number(imc))
+
+  const message = `\nBom..., você nos disse que seu peso é ${weight}kg e sua altura é ${height}m\nEntão ${name}, seu IMC é ${imc} e você se enquadra na categoria: '${category}'.`
+
+  console.log(message);
 }
 
 calcImc();
