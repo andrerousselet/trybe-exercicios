8750fb52a5f9b52d48d604df9ab1e0687cfd57d6
Andre Rousselet <andre098@hotmail.com>
2022-07-27 18:58:50 -0300

    exercise 7

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/raffle/sorteio.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync');
+
+function raffle() {
+  function randomNumber() {
+    const num = Math.floor(Math.random() * 10);
+    return num;
+  }
+  
+  const chosenNum = readline.questionInt('\nEscolha um número de 0 a 10: ');
+  
+  const randomNum = randomNumber();
+
+  const result = chosenNum === randomNum 
+    ? console.log('Parabéns, número correto!')
+    : console.log(`Opa, não foi dessa vez. O número era ${randomNum}`);
+
+  const playAgain = readline.question('\nQuer jogar novamente? Digite s para sim ou qualquer outra coisa para não.');
+
+  return playAgain === 's' && raffle();
+}
+
+raffle();
