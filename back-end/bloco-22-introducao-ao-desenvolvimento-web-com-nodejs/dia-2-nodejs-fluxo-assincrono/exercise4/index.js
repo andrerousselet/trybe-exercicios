e5df55a6b7d44ecd44edca0d18e5196c758adf84
Andre Rousselet <andre098@hotmail.com>
2022-08-01 20:24:16 -0300

    exer4-3: remove simpson by id and modify json file

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise4/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise4/index.js
@@ -21,6 +21,19 @@ async function findSimpsonById(id) {
   return simpson;
 }
 
-findSimpsonById(0)
-  .then(response => console.log(response))
-  .catch(error => console.log(error.message));
+// findSimpsonById(0)
+//   .then(response => console.log(response))
+//   .catch(error => console.log(error.message));
+
+async function removeSimpsonById(id) {
+  try {
+    const simpsonsJSON = await fs.readFile('./simpsons.json', 'utf-8');
+    const parsedSimpsons = JSON.parse(simpsonsJSON);
+    const simpsons = parsedSimpsons.filter((simpson) => Number(simpson.id) !== id);
+    await fs.writeFile('./simpsons.json', JSON.stringify(simpsons));
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+removeSimpsonById(11);
