12c0efba35e5b75957b2adc8aeceeabd3d9564c7
Andre Rousselet <andre098@hotmail.com>
2022-08-01 19:18:53 -0300

    exer1: reject promisse with conditional or resolve it

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise1/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise1/index.js
@@ -3,9 +3,12 @@ function numbers(a, b, c) {
     if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
       return reject(new Error('Informe apenas números'));
     }
+    const result = (a + b) * c;
+    if (result < 50) return reject(new Error('Valor muito baixo'));
+    resolve(result);
   })
 }
 
-numbers(1, 2, '1')
+numbers(1, 4, 9)
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
