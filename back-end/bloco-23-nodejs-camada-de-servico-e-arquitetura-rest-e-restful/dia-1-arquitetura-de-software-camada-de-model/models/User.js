232fe31c5a3b1a466a4ea25ac4dbf888763b18c7
Andre Rousselet <andre098@hotmail.com>
2022-08-09 13:02:40 -0300

    feat: add serialize method in models and add create function

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/User.js
@@ -1,10 +1,27 @@
 const connection = require('./connection')
 
+function serialize (user) {
+  return {
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    email: user.email,
+    password: user.password
+  }
+}
+
 async function getAll () {
   const [users] = await connection.execute(
     'SELECT id, first_name, last_name, email, password FROM model_ex.users;'
   )
-  return users
+  return users.map(serialize)
 }
 
-module.exports = { getAll }
+async function create (firstName, lastName, email, password) {
+  await connection.execute(
+    'INSERT INTO model_ex.users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)',
+    [firstName, lastName, email, password]
+  )
+}
+
+module.exports = { getAll, create }
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/server.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/server.js
@@ -7,9 +7,16 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (_req, res) => res.status(200).send('Funcionando perfeitamente!'))
+
 app.get('/users', async (_req, res) => {
   const users = await User.getAll()
-  res.status(200).json(users)
+  return res.status(200).json(users)
+})
+
+app.post('/users', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  await User.create(firstName, lastName, email, password)
+  return res.status(201).json({ message: 'Usuário criado com sucesso!' })
 })
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
