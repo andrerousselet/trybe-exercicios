abc656857849380183045331b652cfb85dc43f7e
Andre Rousselet <andre098@hotmail.com>
2022-08-23 13:29:07 -0300

    exercicios 6, 7 e 8

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios-do-dia/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios-do-dia/controllers/BooksController.js
@@ -5,6 +5,14 @@ const getAll = async (_req, res) => {
   return res.status(200).json(books);
 }
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const book = await BooksService.getById(id);
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+  return res.status(200).json(book);
+}
+
 module.exports = {
   getAll,
+  getById
 }
