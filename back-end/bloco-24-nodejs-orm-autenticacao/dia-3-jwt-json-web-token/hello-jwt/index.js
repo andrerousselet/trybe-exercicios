ac7771aae473132264a64149c21b2090b979874c
Andre Rousselet <andre098@hotmail.com>
2022-08-23 11:59:34 -0300

    exercicio 3

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/hello-jwt/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/hello-jwt/index.js
@@ -23,6 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
 
+app.get('/users/me', middlewares.auth, controllers.me);
+
 app.post('/login', controllers.login)
 
 app.use(middlewares.error);
