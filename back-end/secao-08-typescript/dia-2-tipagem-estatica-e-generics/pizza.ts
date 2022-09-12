2090172923741140961e840f159610cd092d4b60
Andre Rousselet <andre098@hotmail.com>
2022-09-12 11:44:53 -0300

    feat(exercise4): add new pizza types and create new interfaces extending Pizza

--- back-end/secao-08-typescript/dia-2-tipagem-estatica-e-generics/pizza.ts
+++ back-end/secao-08-typescript/dia-2-tipagem-estatica-e-generics/pizza.ts
@@ -1,23 +1,57 @@
 type Slices = 4 | 6 | 8;
 
+type Common = 'calabresa' | 'frango' | 'pepperoni';
+type Veg = 'marguerita' | 'palmito' | 'cogumelo';
+type Sweet = 'nutela com banana' | 'romeu e julieta' | 'chocolate com morango';
+
 interface Pizza {
   flavour: string,
   slices: Slices,
 }
 
-const calabresa: Pizza = {
-  flavour: 'calabresa',
+// const calabresa: Pizza = {
+//   flavour: 'calabresa',
+//   slices: 8,
+// }
+
+// const marguerita: Pizza = {
+//   flavour: 'marguerita',
+//   slices: 6,
+// }
+
+// const nutela: Pizza = {
+//   flavour: 'nutela',
+//   slices: 4,
+// }
+
+// console.log([calabresa, marguerita, nutela]);
+
+interface CommonPizza extends Pizza {
+  flavour: Common;
+}
+
+interface VegPizza extends Pizza {
+  flavour: Veg;
+}
+
+interface SweetPizza extends Pizza {
+  flavour: Sweet;
+  slices: 4;
+}
+
+const frango: CommonPizza = {
+  flavour: "frango",
   slices: 8,
 }
 
-const marguerita: Pizza = {
-  flavour: 'marguerita',
+const palmito: VegPizza = {
+  flavour: "palmito",
   slices: 6,
 }
 
-const nutela: Pizza = {
-  flavour: 'nutela',
+const romeuEJulieta: SweetPizza = {
+  flavour: "romeu e julieta",
   slices: 4,
 }
 
-console.log([calabresa, marguerita, nutela]);
+console.log([frango, palmito, romeuEJulieta]);
