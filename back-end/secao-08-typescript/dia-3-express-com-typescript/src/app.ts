c93a211d6e63d2d3e0cc2d34dbd34ec831a320eb
Andre Rousselet <andre098@hotmail.com>
2022-09-13 13:07:28 -0300

    feat(app.ts): add example endpoint GET /

--- back-end/secao-08-typescript/dia-3-express-com-typescript/src/app.ts
+++ back-end/secao-08-typescript/dia-3-express-com-typescript/src/app.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { Response } from 'express';
 import 'express-async-errors'
 import errorMiddleware from './middlewares/error';
 
 const app = express();
 
 app.use(express.json())
+
+app.get('/', (_req, res: Response<string>) => res.send('Express + TypeScript'))
 
 app.use(errorMiddleware);
 
