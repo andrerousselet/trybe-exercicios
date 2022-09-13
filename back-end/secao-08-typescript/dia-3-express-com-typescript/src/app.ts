1de75929f41aeb9826015ab74976956a33dadceb
Andre Rousselet <andre098@hotmail.com>
2022-09-13 15:27:00 -0300

    feat(app-and-server): add GET /users endpoint

--- back-end/secao-08-typescript/dia-3-express-com-typescript/src/app.ts
+++ back-end/secao-08-typescript/dia-3-express-com-typescript/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Response } from 'express';
 import 'express-async-errors'
+import * as userController from './controllers/users';
 import errorMiddleware from './middlewares/error';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json())
 
 app.get('/', (_req, res: Response<string>) => res.send('Express + TypeScript'))
+app.get('/users', userController.getAll)
 
 app.use(errorMiddleware);
 
--- back-end/secao-08-typescript/dia-3-express-com-typescript/src/server.ts
+++ back-end/secao-08-typescript/dia-3-express-com-typescript/src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
-	console.log(`Application running on port ${PORT} 🚀`);
+	console.log(`Application running on port ${PORT}`);
 });
