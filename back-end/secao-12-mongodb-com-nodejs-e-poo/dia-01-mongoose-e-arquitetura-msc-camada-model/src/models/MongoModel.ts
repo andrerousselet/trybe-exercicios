ce7f76e82a17aeced2d9bc6c6e10af20411d666f
Andre Rousselet <andre098@hotmail.com>
2022-10-19 16:44:15 -0300

    feat(model): add basic structure of generic model (abstract)

---
+++ back-end/secao-12-mongodb-com-nodejs-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/src/models/MongoModel.ts
@@ -0,0 +1,12 @@
+import { Model } from 'mongoose';
+import IModel from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+}
+
+export default MongoModel;
