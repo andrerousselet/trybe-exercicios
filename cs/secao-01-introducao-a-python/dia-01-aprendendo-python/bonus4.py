57d1d75ffe2aae8571e48ff9945e60a4c4603f62
Andre Rousselet <andre098@hotmail.com>
2022-11-27 15:37:32 -0300

    refactor(exercise 4): variables in one line

--- cs/secao-01-introducao-a-python/dia-01-aprendendo-python/bonus4.py
+++ cs/secao-01-introducao-a-python/dia-01-aprendendo-python/bonus4.py
@@ -1,12 +1,6 @@
 def fuel_price(liters, fuel_type):
-    fuel_prices = {
-      "A": 1.9,
-      "G": 2.5,
-    }
-    discounts = {
-      "A": (0.97, 0.95),
-      "G": (0.96, 0.94),
-    }
+    fuel_prices = {"A": 1.9, "G": 2.5}
+    discounts = {"A": (0.97, 0.95), "G": (0.96, 0.94)}
     total = liters * fuel_prices[fuel_type]
     if liters <= 20:
         return total * discounts[fuel_type][0]
