4b322ff3c526e007d157ebc0fb8b49b1eb470c7c
Andre Rousselet <andre098@hotmail.com>
2023-01-30 17:23:58 -0300

    exercicios do dia 5.1

---
+++ cs/secao-05-estrutura-de-dados-1/dia-01-arquitetura-de-computadores/pc/main_memory.py
@@ -0,0 +1,14 @@
+class MainMemory:
+    def __init__(self):
+        self.clean()
+
+    def load(self, value):
+        self.loaded_memory.append(value)
+
+    def get(self, index):
+        if not float(self.loaded_memory[index]):
+            return 0
+        return float(self.loaded_memory[index])
+
+    def clean(self):
+        self.loaded_memory = []
